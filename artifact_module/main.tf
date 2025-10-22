9f90bf4782ecd02eef75f0eb68f2d6ad206fdff6
90106127 <90106127@email.com>
2025-10-22 06:04:17 +0000

    update

--- artifact_module/main.tf
+++ artifact_module/main.tf
@@ -9,23 +9,21 @@ data "archive_file" "zip_cloud_function" {
   output_path = "${local.fc_prefix_name}.zip"
 }
 
-resource "google_storage_bucket_object" "obj_function" {
-  name = "${local.fc_prefix_name}.zip#${data.archive_file.zip_cloud_function.output_md5}"
-  #  name       = "${local.fc_prefix_name}.zip"
+resource "google_storage_bucket_object" "obj_app_code" {
+  name       = "${local.fc_prefix_name}.zip#${data.archive_file.zip_cloud_function.output_md5}"
   bucket     = var.bucket_source
   source     = data.archive_file.zip_cloud_function.output_path
   depends_on = [data.archive_file.zip_cloud_function]
 }
 
-resource "null_resource" "cloudbuild_trigger" {
-  # Usamos un timestamp para forzar la ejecución en cada 'apply'
+resource "null_resource" "cloudbuild_build_trigger" {
   triggers = {
-    always_run = timestamp()
+    always_run = "${data.archive_file.zip_cloud_function.output_md5}"
   }
 
   provisioner "local-exec" {
-    command = "gcloud builds submit . --config cloudbuild.yaml --project=\"${var.project}\" --substitutions=_AR_REGION=\"${var.region}\",_REPOSITORY_NAME=\"${var.repository_name}\",_IMAGE_NAME=\"${var.image_name}\",SHORT_SHA=${substr(sha1(timestamp()), 0, 7)}"
+    command = "gcloud builds submit --no-source --config ${path.module}/cloudbuild.yaml --project=\"${var.project}\" --substitutions=_AR_REGION=\"${var.region}\",_REPOSITORY_NAME=\"${var.repository_name}\",_IMAGE_NAME=\"${var.image_name}\",SHORT_SHA=${data.archive_file.zip_cloud_function.output_md5},_BUCKET_SOURCE=\"${var.bucket_source}\",_ZIP_OBJECT=\"${google_storage_bucket_object.obj_app_code.name}\""
   }
 
-  depends_on = [google_storage_bucket_object.obj_function]
+  depends_on = [google_storage_bucket_object.obj_app_code]
 }
--- artifact_module/output.tf
+++ artifact_module/output.tf
@@ -1,4 +1,4 @@
 output "artifact_registry_url" {
   description = "URL del repositorio de Artifact Registry para la imagen Docker"
-  value       = "${var.region}-docker.pkg.dev/${var.project}/${var.repository_name}"
+  value       = "${var.region}-docker.pkg.dev/${var.project}/${var.repository_name}/${var.image_name}:${data.archive_file.zip_cloud_function.output_md5}"
 }
